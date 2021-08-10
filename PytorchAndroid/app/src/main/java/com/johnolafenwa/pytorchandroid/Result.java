17a4a7148e8311bc9d3595d7d22faa6d9cae58bc
Shiva R. Iyer <sri223@nyu.edu>
2021-08-10 16:40:22 -0400

    [MAJOR] Request permissions for using location
    - Added a new resource to ref result activity layout
    - Removed unused resources in strings.xml

--- PytorchAndroid/app/src/main/java/com/johnolafenwa/pytorchandroid/Result.java
+++ PytorchAndroid/app/src/main/java/com/johnolafenwa/pytorchandroid/Result.java
@@ -1,21 +1,35 @@
 package com.johnolafenwa.pytorchandroid;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.location.LocationManager;
 import android.location.Location;
-import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Result extends AppCompatActivity {
+public class Result extends AppCompatActivity 
+    implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private static final int LOC_REQUEST_CODE = 002;
+
+    private View resLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+	resLayout = findViewById(R.id.result_layout);
+	
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -33,9 +47,56 @@ public class Result extends AppCompatActivity {
         TextView textView_date = findViewById(R.id.date);
         textView_date.setText(date);
 
-	// TODO: request user for location permission here
+	// get the location info 
+	checkShowLocationInfo();
 
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE); 
+	// TODO: request user for storage permission and store the resulting image
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == LOC_REQUEST_CODE) {
+            // Request for location permission
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted
+                showLocationInfo();
+            } else {
+                // Permission request was denied
+                Snackbar.make(resLayout, R.string.loc_permission_denied, Snackbar.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void checkShowLocationInfo() {
+	// TODO: request user for location permission here
+	if (ActivityCompat.checkSelfPermission(Result.this, Manifest.permission.ACCESS_FINE_LOCATION) 
+	    == PackageManager.PERMISSION_GRANTED) {
+	    // permission is already available
+	    showLocationInfo();
+	} else if (ActivityCompat.shouldShowRequestPermissionRationale(Result.this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+	    // provide a rationale and request permission
+	    Snackbar.make(resLayout, R.string.loc_access_required, Snackbar.LENGTH_INDEFINITE)
+		.setAction(R.string.ok, new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+			    // request the permission
+			    requestLocationPermission();
+			}
+		    }).show();
+	} else {
+	    // permission is not available and must be requested
+	    Snackbar.make(resLayout, R.string.loc_access_required, Snackbar.LENGTH_SHORT).show();
+	    requestLocationPermission();
+	}
+    }
+
+    private void requestLocationPermission() {
+	ActivityCompat.requestPermissions(Result.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOC_REQUEST_CODE);
+    }
+
+    private void showLocationInfo() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         double longitude = location.getLongitude();
         double latitude = location.getLatitude();
@@ -45,8 +106,5 @@ public class Result extends AppCompatActivity {
 
         TextView textView_loc = findViewById(R.id.loc);
         textView_loc.setText(loc);
-
-	// TODO: request user for storage permission and store the resulting image
     }
-
 }
